ddb71aea353b5edd7d6f6406deba62982147f711
Justin Williams <justin.williams1597@yahoo.com>
2018-10-17 00:34:18 -0400

    Added the ability to display HTML to describe error

--- com.googlecode.cppcheclipse.ui/src/com/vaccs/ui/VACCSView.java
+++ com.googlecode.cppcheclipse.ui/src/com/vaccs/ui/VACCSView.java
@@ -1,28 +1,40 @@
 package com.vaccs.ui;
 
 
+import java.io.File;
+import java.io.IOException;
+import java.lang.reflect.Field;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import org.eclipse.cdt.utils.Platform;
+import org.eclipse.core.runtime.FileLocator;
+import org.eclipse.core.runtime.Path;
+import org.eclipse.jface.viewers.ITableLabelProvider;
+import org.eclipse.jface.viewers.LabelProvider;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.SWTError;
+import org.eclipse.swt.browser.Browser;
+import org.eclipse.swt.custom.CTabFolder;
+import org.eclipse.swt.custom.CTabItem;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Label;
-import org.eclipse.ui.part.*;
+import org.eclipse.swt.widgets.Text;
+import org.eclipse.ui.part.ViewPart;
 
 import com.googlecode.cppcheclipse.core.CppcheclipsePlugin;
 import com.googlecode.cppcheclipse.ui.Utils;
 import com.vaccs.model.ViewContext;
-
-import org.eclipse.jface.viewers.*;
-import org.eclipse.swt.graphics.Image;
-
-import java.io.File;
-import java.lang.reflect.Field;
-
-import org.eclipse.jface.action.*;
-import org.eclipse.jface.dialogs.MessageDialog;
-import org.eclipse.ui.*;
-import org.eclipse.swt.widgets.Menu;
-import org.eclipse.swt.SWT;
-import org.eclipse.swt.events.SelectionEvent;
-import org.eclipse.swt.events.SelectionListener;
 
 
 /**
@@ -50,16 +62,17 @@ public class VACCSView extends ViewPart {
 	 */
 	public static final String ID = "com.vaccs.ui.view";
 
-	
-	private Action action1;
-	private Action action2;
-	private Action doubleClickAction;
-	
 	private Label errorLabel;
 	private Button gotoURL;
 	
-	private String errorID;
-	 
+	private CTabFolder tabFolder;
+	private CTabItem cTabItem[] = new CTabItem[4];
+	
+	private Browser explanationBrowser;
+	private Browser exampleBrowser;
+	private Browser exploitBrowser;
+	
+
 
 	class ViewLabelProvider extends LabelProvider implements ITableLabelProvider {
 		@Override
@@ -78,37 +91,36 @@ public class VACCSView extends ViewPart {
 
 	@Override
 	public void createPartControl(Composite parent) {
+		parent.setLayout(new GridLayout(1, true));
+		GridData data = new GridData(SWT.FILL, SWT.TOP, true, false, 1, 1);
+		errorLabel = new Label(parent,0);
+		errorLabel.setLayoutData(data);
 		
-		errorLabel = new Label(parent, 0);
-		gotoURL = new Button(parent, 0);
-		gotoURL.setText("relevant video");
-		gotoURL.addSelectionListener(new SelectionListener () {
-
-			@Override
-			public void widgetSelected(SelectionEvent e) {
-				String url = null;
-				try {
-					Field field = VideoURL.class.getField(ViewContext.getInstance().getProblemId());
-					url = (String) field.get(null);
-				} catch (Exception ex) {
-					url = VideoURL.defaultVideo;
-				}
-								
-				try {
-					Utils.openUrl(url);
-				} catch (Exception ex) {
-					CppcheclipsePlugin.logError("Could not open bug report page", ex);
-				}
-			}
-
-			@Override
-			public void widgetDefaultSelected(SelectionEvent e) {
-				// TODO Auto-generated method stub
-				
-			}
-			
-		});
+		tabFolder = new CTabFolder(parent, SWT.BORDER);
+		data = new GridData(SWT.FILL, SWT.FILL, true, true, 1, 1);
+		tabFolder.setLayoutData(data);
+		tabFolder.addSelectionListener(new SelectionAdapter() {
+		      public void widgetSelected(SelectionEvent event) {
+		          if(tabFolder.getSelection().equals(cTabItem[3])) {
+		        	  String url = null;
+					  try {
+						Field field = VideoURL.class.getField(ViewContext.getInstance().getProblemId());
+						url = (String) field.get(null);
+					  } catch (Exception ex) {
+						url = VideoURL.defaultVideo;
+					  }
+										
+					  try {
+						Utils.openUrl(url);
+					  } catch (Exception ex) {
+						CppcheclipsePlugin.logError("Could not open video page", ex);
+					  }
+		          }
+		        }
+		      });
 		
+		setupTabFolder();
+//		initializeInformation();
 		
 
 		// Create the help context id for the viewer's control
@@ -118,77 +130,73 @@ public class VACCSView extends ViewPart {
 //		hookDoubleClickAction();
 //		contributeToActionBars();
 	}
+	
+	private String readFileIntoString(String filePath) {
+		
+		
+		try {
+			URL url = FileLocator.toFileURL(FileLocator.find(Platform.getBundle("com.googlecode.cppcheclipse.ui"), new Path(filePath), null));
+			return new String(Files.readAllBytes(Paths.get(new URI(url.toString().replaceAll(" ", "%20")))));
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return "";
+		} 
+	}
 
-//	private void hookContextMenu() {
-//		MenuManager menuMgr = new MenuManager("#PopupMenu");
-//		menuMgr.setRemoveAllWhenShown(true);
-//		menuMgr.addMenuListener(new IMenuListener() {
-//			public void menuAboutToShow(IMenuManager manager) {
-//				VACCSView.this.fillContextMenu(manager);
-//			}
-//		});
-//		Menu menu = menuMgr.createContextMenu(viewer.getControl());
-//		viewer.getControl().setMenu(menu);
-//		getSite().registerContextMenu(menuMgr, viewer);
-//	}
-//
-//	private void contributeToActionBars() {
-//		IActionBars bars = getViewSite().getActionBars();
-//		fillLocalPullDown(bars.getMenuManager());
-//		fillLocalToolBar(bars.getToolBarManager());
-//	}
-//
-//	private void fillLocalPullDown(IMenuManager manager) {
-//		manager.add(action1);
-//		manager.add(new Separator());
-//		manager.add(action2);
-//	}
-//
-//	private void fillContextMenu(IMenuManager manager) {
-//		manager.add(action1);
-//		// Other plug-ins can contribute there actions here
-//		manager.add(new Separator(IWorkbenchActionConstants.MB_ADDITIONS));
-//	}
-//	
-//	private void fillLocalToolBar(IToolBarManager manager) {
-//		manager.add(action1);
-//		manager.add(action2);
-//	}
-//
-//	private void makeActions() {
-//		action1 = new Action() {
-//			//link to video
-//		};
-//		action1.setText("Action 1");
-//		action1.setToolTipText("Action 1 tooltip");
-//		action1.setImageDescriptor(PlatformUI.getWorkbench().getSharedImages().
-//			getImageDescriptor(ISharedImages.IMG_OBJS_INFO_TSK));
-//		
-//	}
-//
-//	private void hookDoubleClickAction() {
-//		viewer.addDoubleClickListener(new IDoubleClickListener() {
-//			public void doubleClick(DoubleClickEvent event) {
-//				doubleClickAction.run();
-//			}
-//		});
-//	}
-//	private void showMessage(String message) {
-//		MessageDialog.openInformation(
-//			viewer.getControl().getShell(),
-//			"Sample View",
-//			message);
-//	}
-//
-//	@Override
-//	public void setFocus() {
-//		viewer.getControl().setFocus();
-//	}
+	private void setupTabFolder() {
+		
+		cTabItem[0] = new CTabItem(tabFolder, SWT.NONE);
+		cTabItem[0].setText("What is this?");
+		try {
+			explanationBrowser = new Browser(tabFolder, SWT.NONE);
+			cTabItem[0].setControl(explanationBrowser);
+		} catch (SWTError e) {
+			System.out.println("Could not instantiate Browser: " + e.getMessage());
+			Text text = new Text(tabFolder, SWT.BORDER);
+	        text.setText("Could not load browser");
+	        cTabItem[0].setControl(text);
+			return;
+		}
+        
+        cTabItem[1] = new CTabItem(tabFolder, SWT.NONE);
+        cTabItem[1].setText("Examples");
+        try {
+        	exampleBrowser = new Browser(tabFolder, SWT.NONE);
+        	cTabItem[1].setControl(exampleBrowser);
+		} catch (SWTError e) {
+			System.out.println("Could not instantiate Browser: " + e.getMessage());
+			Text text = new Text(tabFolder, SWT.BORDER);
+	        text.setText("Could not load browser");
+	        cTabItem[1].setControl(text);
+			return;
+		}
+        
+        cTabItem[2] = new CTabItem(tabFolder, SWT.NONE);
+        cTabItem[2].setText("Exploits");
+        try {
+        	exploitBrowser = new Browser(tabFolder, SWT.NONE);
+        	cTabItem[2].setControl(exploitBrowser);
+		} catch (SWTError e) {
+			System.out.println("Could not instantiate Browser: " + e.getMessage());
+			Text text = new Text(tabFolder, SWT.BORDER);
+	        text.setText("Could not load browser");
+	        cTabItem[2].setControl(text);
+			return;
+		}
+        
+        cTabItem[3] = new CTabItem(tabFolder, SWT.NONE);
+        cTabItem[3].setText("Video");
+	}
 	
 	
 	public void dataChanged() {
 		System.out.printf("problemId %s line %d fileName %s\n", ViewContext.getInstance().getProblemId(), ViewContext.getInstance().getLineNumber(), ViewContext.getInstance().getFile().getName());
 		errorLabel.setText(ViewContext.getInstance().getProblemId());
+		explanationBrowser.setText(readFileIntoString("./html/explanation/" + ViewContext.getInstance().getProblemId() + ".html"));
+		exampleBrowser.setText(readFileIntoString("./html/example/" + ViewContext.getInstance().getProblemId() + ".html"));
+		exploitBrowser.setText(readFileIntoString("./html/exploits/" + ViewContext.getInstance().getProblemId() + ".html"));
+		tabFolder.setSelection(cTabItem[0]);
 	}
 
 	@Override
